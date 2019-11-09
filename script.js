a775904ec3d7107b90450b6eb9b6a256d2abfee3
AnAstutehuMan <51687055+AnAstutehuMan@users.noreply.github.com>
2019-11-09 15:54:33 -0700

    beutified it again

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 var map;
 var input;
-function load(){
+
+function load() {
     input = document.getElementById('Dest');
-    map = L.map('mainmap').setView([0,0],5);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a       href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',maxZoom: 18,id: 'mapbox.streets',accessToken: 'pk.eyJ1IjoibHlubmxvIiwiYSI6ImNrMnMxcGJoNDB2dTMzbm1ieG53dHBrd3QifQ.Rd6I5PvAtpo_ni7AfRNH1w'}).addTo(map);
-     L.control.locate(icon='location.jpg').addTo(map);
+    map = L.map('mainmap').setView([0, 0], 5);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a       href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibHlubmxvIiwiYSI6ImNrMnMxcGJoNDB2dTMzbm1ieG53dHBrd3QifQ.Rd6I5PvAtpo_ni7AfRNH1w'
+    }).addTo(map);
+    L.control.locate(icon = 'location.jpg').addTo(map);
 }
-function pop(location){
+
+function pop(location) {
     L.marker(location).addTo(map).bindPopup('Selected location').openPopup();
 }
 
-function send(){
+function send() {
     loc = input.value.split(',');
     console.log(loc)
     pop(loc);
