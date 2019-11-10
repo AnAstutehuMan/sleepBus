438e42706457ea26e0514e8962453542a04a5e42
baelany <34193501+baelany@users.noreply.github.com>
2019-11-09 21:53:40 -0700

    truncated distance to 2 decimal places

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function load() {
             navigator.geolocation.getCurrentPosition(UpdateDistance);
         }
         ghostmarker = e;
-        loc = e.latlng.lat + " , " + e.latlng.lng;
+        loc = e.latlng.lat + ', ' + e.latlng.lng;
         input.innerHTML = loc;
     })
 }
@@ -74,8 +74,8 @@ function getDistance(lat1, lon1, lat2, lon2) {
     var dLon = deg2rad(lon2 - lon1);
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c; // Distance in km
-    return d * 0.621371; // Distance in miles
+    var d = R*c*0.621371; // Distance in miles
+    return d.toFixed(2); // Truncates to 2 decimal places
 }
 
 function ComputeDistance(current) {
