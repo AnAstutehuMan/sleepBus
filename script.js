44031b0c690e17a26fa1e4e893733a8ab2fe8254
lynnlo <tharlynnhtet.ong@gmail.com>
2019-11-09 17:51:48 -0700

    Updated Tracker

--- script.js
+++ script.js
@@ -55,13 +55,9 @@ function getDistance(lat1,lon1,lat2,lon2) {
     var R = 6371; // Radius of the earth in km
     var dLat = deg2rad(lat2 - lat1); // deg2rad below
     var dLon = deg2rad(lon2 - lon1);
-    var a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-        Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    var a =Math.sin(dLat / 2) * Math.sin(dLat / 2) +Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *Math.sin(dLon / 2) * Math.sin(dLon / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
     var d = R * c; // Distance in km
-    var d = d / 1000 //Distance in meters
     return d;
   }
   
@@ -80,8 +76,8 @@ function ComputeDistance(current){
     lat1 = current.coords.latitude // Lat of Current Position
     lng1 = current.coords.longitude // Lng of Current Position
     if (marker != 'undf') {
-        lat2 = marker.latlng.lat // Lat of Marker
-        lng2 = marker.latlng.lng // Lng of Marker
+        lat2 = marker._latlng.lat // Lat of Marker
+        lng2 = marker._latlng.lng // Lng of Marker
         console.log(getDistance(lat1,lng1,lat2,lng2))
     }
     else{
